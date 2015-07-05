7f2646c35aa8b06bdd8c0e3642e2da11b127960b
Michael Scholtz <michael.scholtz@outlook.com>
2015-07-05 09:53:08 +0200

    Refactoring.

---
+++ friendslistmanager.js
@@ -0,0 +1,87 @@
+var fs = require('fs');
+
+module.exports = FriendsListManager;
+
+/**
+ * Friends list management class.
+ * @module
+ */
+function FriendsListManager() {
+    this.friends = {};
+}
+
+/**
+ * Loads friends list from the specified path.
+ * @param  {string} path Path to friends list JSON file.
+ */
+FriendsListManager.prototype.load = function(path) {
+    var friendsListManager = this;
+
+    if(fs.existsSync(path))
+        friendsListManager.friends = JSON.parse(fs.readFileSync(path));
+
+    friendsListManager.friends = {};
+};
+
+/**
+ * Saves friends list to output file.
+ * @param  {string} path Path to output file.
+ */
+FriendsListManager.prototype.save = function(path) {
+    var friends = this.friends;
+
+    fs.writeFileSync(path, JSON.stringify(friends));
+};
+
+/**
+ * Returns oldest friend.
+ * @return {string} SteamID64.
+ */
+FriendsListManager.prototype.getOldestAdded = function() {
+    var id;
+    var lowest = Number.MAX_VALUE;
+    var friends = this.friends;
+
+    for(var steamID in friends) {
+        if(friends[steamID] < lowest) {
+            lowest = friends[steamID];
+            id = steamID;
+        }
+    }
+
+    return id;
+};
+
+/**
+ * Adds friend to managers internal friends list.
+ * @param  {string} steamID User's SteamID64.
+ */
+FriendsListManager.prototype.add = function(steamID) {
+    this.friends[steamID] = this._getTimeStamp();
+};
+
+/**
+ * Removes friend from managers internal friends list.
+ * @param  {string} steamID User's SteamID64.
+ */
+FriendsListManager.prototype.remove = function(steamID) {
+    delete this.friends[steamID];
+};
+
+/**
+ * Returns number of friends.
+ * @return {Number} Number of friends.
+ */
+FriendsListManager.prototype.count = function() {
+    var friends = this.friends;
+
+    return Object.keys(friends).length;
+};
+
+/**
+ * Returns current timestamp.
+ * @return {Number} Current timestamp.
+ */
+FriendsListManager.prototype._getTimeStamp = function() {
+    return Math.floor(new Date() / 1000);
+};
--- index.js
+++ index.js
@@ -1,76 +1,41 @@
 var fs = require('fs');
+var FriendsListManager = require('./friendslistmanager.js');
 
 /**
  * Exported function.
  * @param  {object} VaporAPI VaporAPI instance.
  */
 module.exports = function(VaporAPI) {
+    var manager = new FriendsListManager();
+
     var client = VaporAPI.getClient();
     var Steam = VaporAPI.getSteam();
     var config = VaporAPI.getConfig();
     var log = VaporAPI.getLogger();
 
-    var friendsList = {};
     var FRIENDSLIST_PATH = VaporAPI.getDataFolderPath() + '/friendslist.json';
 
     var limit = config.limit || 100;
     var welcomeMessage = config.welcomeMessage || undefined;
 
+    /**
+     * Adds friend to friends list.
+     *
+     * Also removes friend if necessary.
+     * @param {object} client Active Steam client.
+     * @param {string} user   SteamID64.
+     */
+    function addFriend(client, user) {
+        if(manager.count() === limit) {
+            var removedUser = manager.getOldestAdded();
+            client.removeFriend(removedUser);
+            manager.remove(removedUser);
 
-    function loadFriendsList(path) {
-        if(fs.existsSync(path))
-            return JSON.parse(fs.readFileSync(path));
-
-        return {};
-    }
-
-
-    function saveFriendsList(path, friendsList) {
-        fs.writeFileSync(path, JSON.stringify(friendsList));
-    }
-
-
-    function getOldestActive(friendsList) {
-        var id;
-        var lowest = Number.MAX_VALUE;
-
-        for(var steamID in friendsList) {
-            if(friendsList[steamID] < lowest) {
-                lowest = friendsList[steamID];
-                id = steamID;
-            }
-        }
-
-        return id;
-    }
-
-
-    function addToFriendsList(friendsList, steamID) {
-        friendsList[steamID] = getTimeStamp();
-    }
-
-
-    function removeFromFriendsList(friendsList, steamID) {
-        delete friendsList[steamID];
-    }
-
-
-    function countFriendsList(friendsList) {
-        return Object.keys(friendsList).length;
-    }
-
-
-    function addFriend(client, user, friendsList) {
-        if(countFriendsList(friendsList) === limit) {
-            var friend = getOldestActive(friendsList);
-            client.removeFriend(friend);
-            removeFromFriendsList(friendsList, friend);
-
-            log.info("My friends list was full. " + client.users[user].playerName + " (" + user + ") has been removed.");
+            log.info("My friends list was full. " + client.users[removedUser].playerName + " (" + removedUser + ") has been removed.");
         }
 
         client.addFriend(user);
-        addToFriendsList(friendsList, user);
+        manager.add(user);
 
         log.info("User " + user + " has been added to my friends list.");
 
@@ -80,14 +45,10 @@ module.exports = function(VaporAPI) {
     }
 
 
-    function getTimeStamp() {
-        return Math.floor(new Date() / 1000);
-    }
-
     /**
      * Main entry point
      */
-    friendsList = loadFriendsList(FRIENDSLIST_PATH);
+    manager.load(FRIENDSLIST_PATH);
 
     // Handle 'friend' event
     VaporAPI.registerHandler({
@@ -96,12 +57,12 @@ module.exports = function(VaporAPI) {
         },
         function(user, type) {
             if(type === Steam.EFriendRelationship.RequestRecipient) {
-                addFriend(client, user, friendsList);
+                addFriend(client, user);
             } else if(type === Steam.EFriendRelationship.None) {
-                removeFromFriendsList(friendsList, user);
+                manager.remove(user);
             }
 
-            saveFriendsList(FRIENDSLIST_PATH, friendsList);
+            manager.save(FRIENDSLIST_PATH);
         }
     );
 
@@ -113,22 +74,22 @@ module.exports = function(VaporAPI) {
         function() {
             for(var user in client.friends) {
                 if(client.friends[user] === Steam.EFriendRelationship.Friend) {
-                    if(!friendsList.hasOwnProperty(user)) {
-                        addToFriendsList(friendsList, user);
+                    if(!manager.friends.hasOwnProperty(user)) {
+                        manager.add(user);
                     }
                 } else if(client.friends[user] === Steam.EFriendRelationship.RequestRecipient) {
-                    addFriend(client, user, friendsList);
+                    addFriend(client, user);
                 }
             }
 
             // 'friendsList' may still contain dead entries
-            for(var friend in friendsList) {
+            for(var friend in manager.friends) {
                 if(client.friends[friend] === undefined) {
-                    removeFromFriendsList(friendsList, friend);
+                    manager.remove(friend);
                 }
             }
 
-            saveFriendsList(FRIENDSLIST_PATH, friendsList);
+            manager.save(FRIENDSLIST_PATH);
 
             log.info("Friends list has been synchronized.");
         }
